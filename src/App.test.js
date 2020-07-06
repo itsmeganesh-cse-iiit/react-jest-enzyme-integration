867eddfb688d946feb932109978ad17212e81233
Ganesh Koiada <ganesh@spheremi.com>
2020-07-06 14:49:51 +0530

    config and shallow

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import {shallow} from 'enzyme'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("AppComponent",()=>{
+  test("Snapshot render",()=>{
+    const component=shallow(<App/>);
+    expect(component).toMatchSnapshot();
+  })
+})
